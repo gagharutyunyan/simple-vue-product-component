dda4643bafbf1bee6c69ebcc2cccf681fdfad743
gagharutyunyan <gagharut@gmail.com>
2020-05-19 07:33:49 +0300

    created main block and 2 childs

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    title: "Product",
+    description: "My best product",
+    img: ["/img/first-sock.jpg", "/img/second-sock.jpg"],
+    isAvailable: true,
+  },
+});
