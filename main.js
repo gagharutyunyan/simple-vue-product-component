b75528e1a719079ee63f8a71a59291718eb6e2df
gagharutyunyan <gagharut@gmail.com>
2020-05-20 15:40:33 +0300

    learned user and inline methods in vue

--- main.js
+++ main.js
@@ -3,12 +3,23 @@ const app = new Vue({
   data: {
     title: "Product",
     description: "My best product",
-    img: ["/img/first-sock.jpg", "/img/second-sock.jpg"],
     inventory: 2,
+    image: "/img/first-sock.jpg",
     details: ["100% cotton", "for kids"],
     variants: [
-      { id: 1, variantColor: "red" },
-      { id: 2, variantColor: "green" },
+      { id: 1, variantColor: "red", img: "/img/first-sock.jpg" },
+      { id: 2, variantColor: "green", img: "/img/second-sock.jpg" },
     ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(img) {
+      if (img === this.image) return;
+      console.log("object");
+      this.image = img;
+    },
   },
 });
