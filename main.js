80022724b39a1a534b5125c9252c4f778479dd27
gagharutyunyan <gagharut@gmail.com>
2020-05-25 16:07:03 +0300

    created simple component and go to next part

--- main.js
+++ main.js
@@ -1,20 +1,74 @@
-const app = new Vue({
-  el: "#app",
-  data: {
-    title: "Product",
-    description: "My best product",
-    selectedItem: 0,
-    details: ["100% cotton", "for kids"],
-    variants: [
-      { id: 1, variantColor: "red", img: "/img/first-sock.jpg", quantity: 10 },
-      {
-        id: 2,
-        variantColor: "green",
-        img: "/img/second-sock.jpg",
-        quantity: 5,
-      },
-    ],
-    cart: 0,
+Vue.component("product", {
+  props: {
+    cost: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `
+  <div class="product">
+        <div class="product_image product_container">
+          <img :src="image" alt="" />
+        </div>
+        <div class="product_info product_container">
+          <h1>{{title}}</h1>
+          <p>{{description}}</p>
+          <ul>
+            <li v-for="detail in details">{{detail}}</li>
+          </ul>
+          <div class="available">
+            <p v-if="inventory > 1">we have {{inventory}} socks</p>
+            <p v-else-if="inventory === 1">we have 1 socks</p>
+            <p v-else>we do not have socks</p>
+            <p >{{cost}}</p>
+          </div>
+          <div class="hover">
+            <div
+              class="hovered_block"
+              v-for="(variant,index) in variants"
+              :key="variant.id"
+              :style="{backgroundColor: variant.variantColor}"
+              @mouseover="updateProduct(index)"
+            ></div>
+          </div>
+          <div class="order">
+            <button
+              class="add_btn"
+              @click="addToCart"
+              :disabled="isDisabled('add')"
+            >
+              Add to cart
+            </button>
+            <button @click="delToCart" :disabled="isDisabled('delete')">
+              Del
+            </button>
+            <div class="cart_btn">Cart({{cart}})</div>
+          </div>
+        </div>
+      </div>
+  `,
+  data() {
+    return {
+      title: "Product",
+      description: "My best product",
+      selectedItem: 0,
+      details: ["100% cotton", "for kids"],
+      variants: [
+        {
+          id: 1,
+          variantColor: "red",
+          img: "/img/first-sock.jpg",
+          quantity: 10,
+        },
+        {
+          id: 2,
+          variantColor: "green",
+          img: "/img/second-sock.jpg",
+          quantity: 5,
+        },
+      ],
+      cart: 0,
+    };
   },
   methods: {
     addToCart() {
@@ -44,6 +98,7 @@ const app = new Vue({
     image() {
       return this.variants[this.selectedItem].img;
     },
+
     inventory() {
       const count = this.variants.reduce((acc, curr) => {
         return acc.quantity + curr.quantity;
@@ -51,4 +106,8 @@ const app = new Vue({
       return count;
     },
   },
+});
+
+const app = new Vue({
+  el: "#app",
 });
